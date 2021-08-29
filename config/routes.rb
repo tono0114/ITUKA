ac29c8bda0f96996894ef80e3ea196f2ba74fbb0
tono0114 <git@github.com:tono0114/PF-ITUKA.git>
2021-08-29 14:19:05 +0000

    Add model,controller,view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  resources :users
+
+
+  devise_scope :user do
+    get "sign_up", :to => "users/registrations#new"
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
+
 end
